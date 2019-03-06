d280468714ea35bcaa3f1f1552ea41ec1fd830fa
Samya Maiti <samya.maiti2012@gmail.com>
2019-03-06 09:37:35 +0530

    Template

--- Template/custom_python_package/data_insight.py
+++ Template/custom_python_package/data_insight.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from IPython.core.interactiveshell import InteractiveShell
 import warnings
 from tqdm import tqdm
@@ -13,29 +12,20 @@ import math
 import itertools
 import os
 
-from sklearn.model_selection import train_test_split, StratifiedKFold, KFold, cross_val_score, GridSearchCV, RepeatedKFold, learning_curve, ShuffleSplit
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.model_selection import StratifiedKFold, KFold, cross_val_score, GridSearchCV, RepeatedKFold, learning_curve, ShuffleSplit
 from sklearn.metrics import roc_curve, auc, accuracy_score,classification_report, recall_score,precision_score,precision_recall_curve,average_precision_score, silhouette_score,roc_curve, auc,confusion_matrix,mean_absolute_error,mean_squared_error,roc_auc_score,f1_score
 from sklearn.preprocessing import binarize
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.cluster import KMeans
 from sklearn import svm
 from xgboost import XGBClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 
 from mlxtend.classifier import StackingClassifier
 
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline as impipe
-
 
 
 
@@ -99,6 +89,53 @@ def get_frequency_of_attr_value(dataF):
     dataF_count = get_unique_value_count_per_attribute(dataF)
     dataF_count_col = dataF_count.reset_index()['index']
     return pd.DataFrame(dataF[dataF_count_col.tolist()].apply(pd.Series.value_counts))
+    
+
+    
+def covert_var(dataF, conversion_type):
+    if(conversion_type=="cat"):
+        converted_dataF = dataF.astype('category', copy=True)
+    if(conversion_type=="num"):
+        converted_dataF = dataF.astype('float64', copy=True)
+    return converted_dataF
+
+
+    
+def get_outlier_removed_by_sigma_dist(dataF, target, targetVal,dist):
+    dataF_target = dataF[dataF[target]==targetVal]
+    dataF_target_1 = dataF_target.drop(target, axis=1)
+    dataF_target_Sigma = dataF_target_1[~((np.abs(((dataF_target_1 - dataF_target_1.mean())/dataF_target_1.std())) > dist).any(1))]
+    return dataF_target_Sigma
+
+
+
+
+def get_vif(dataF):
+    vif = pd.DataFrame()
+    dataF_vif = dataF.dropna()
+    vif["VIF_Factor"] = [variance_inflation_factor(dataF_vif.values, i) for i in range(dataF_vif.shape[1])]
+    vif["features"] = dataF_vif.columns
+    return vif.sort_values('VIF_Factor', ascending=False)
+
+
+def get_missing_value_by_row(dataF):
+    dataF_cull_count_by_row = pd.DataFrame(dataF.isna().sum(axis=1)).rename({0:"count_of_null"}, axis=1).sort_values(
+        by='count_of_null', ascending=False).assign(
+        count_of_null_percent=lambda row:(row.count_of_null/dataF.shape[1])*100)
+    return dataF_cull_count_by_row['count_of_null'].value_counts().sort_index()
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
     
     
     
--- Template/custom_python_package/data_plot.py
+++ Template/custom_python_package/data_plot.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 
 
-def plotDataDistAcrossTarg(dataF, target):
+def plot_data_dist_across_targ(dataF, target):
     """
     Plot target variable distribution
     """
@@ -22,3 +22,36 @@ def plotDataDistAcrossTarg(dataF, target):
     plt.show();
     
     
+
+def get_correlation_plot(dataF):
+    corr = dataF.corr()
+    f, ax = plt.subplots(figsize=(16, 10))
+    sns.heatmap(corr, ax=ax, annot=True, cmap="YlGnBu");
+    
+    
+def distribution_of_var(dataF):
+    plt.figure(figsize=(18,9));
+    plt.title("Distribution of all variables");
+    plt.ylabel("Range of values on Strandard normal scale")
+    plt.xlabel("Variables")
+    dataF.boxplot(rot=90);
+
+    
+    
+def draw_pair_plot(dataF, target):
+    g = sns.PairGrid(dataF, hue=target)
+    g.map_diag(plt.hist)
+    g.map_offdiag(plt.scatter);
+
+    
+
+def draw_pca_plot(dataF):
+    fig = plt.gcf()
+    fig.set_size_inches( 16, 8)
+    sns.set(font_scale = .7)
+    dataF_line_plot = sns.lineplot(x="Number of PC", y="Cumulative Sum of variance", data=dataF)
+    dataF_line_plot.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    dataF_line_plot.xaxis.set_major_formatter(ticker.ScalarFormatter())
+    plt.show()
+
+
